8fcc4b54751f0ca80acf16dfb8ea884a17454854
BadgeFans <rikmmc9@gmail.com>
2025-10-26 19:37:38 -0400

    Delete functions/api/auth.js

--- functions/api/auth.js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const { OAUTH_GITHUB_CLIENT_ID } = context.env;
-  
-  const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${OAUTH_GITHUB_CLIENT_ID}&scope=repo,user`;
-  
-  return Response.redirect(githubAuthUrl, 302);
-}
